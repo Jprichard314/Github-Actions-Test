eb639c84e2965d5dc8abe6f940aba34c45fd00d7
Jonathan Prichard <66090077+Jprichard314@users.noreply.github.com>
2024-05-04 11:39:01 -0400

    test 2
    - Test using submodules to prep for more complex reporting workflows

---
+++ R/test2.R
@@ -0,0 +1,8 @@
+source('R/rUtils/dataCapture/apis.R')
+
+test2 <- getData_phlCartoApi("select * from PUBLIC_CASES_FC limit 10")
+
+write.csv(test2,paste0('data/',
+                       'Test_2_',
+                       format.POSIXct(Sys.time(), "%Y-%m-%d %H_%M_%S"),
+                       '_data.csv'))
